c5fba716c2bfb8d392ef47107a22f86d27df59be
Lukasz Laszko <lukaszlaszko@gmail.com>
2020-01-27 21:02:00 +0800

    files renamed

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(
     name='insideairbnb_data',
     version='0.0.0.9000',
-    scripts=['download.py'],
+    scripts=['download_airbnb.py'],
     install_requires = [
         'beautifulsoup4',
         'loguru',
